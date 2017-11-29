ea379d3d0ac971225c05d4e762ae4f093f4666b7
root <kdquon@ucdavis.edu>
2017-11-28 17:20:16 -0800

    Updated GithubQueirier. Only tracks PushEvents and commits (sha + message)

--- src/main/java/ecs189/querying/github/GithubQuerier.java
+++ src/main/java/ecs189/querying/github/GithubQuerier.java
@@ -27,6 +27,16 @@ public class GithubQuerier {
             JSONObject event = response.get(i);
             // Get event type
             String type = event.getString("type");
+            // Get commit sha and message
+            JSONObject payload = event.getJSONObject("payload");
+            JSONArray commits = payload.getJSONArray("commits");
+            List<String> sha = new ArrayList<String>();
+            List<String> message = new ArrayList<String>();
+            for(int j = 0; j < commits.length(); j++) {
+                JSONObject commit = commits.getJSONObject(0);
+                sha.add(commit.getString("sha"));
+                message.add(commit.getString("message"));
+            }
             // Get created_at date, and format it in a more pleasant style
             String creationDate = event.getString("created_at");
             SimpleDateFormat inFormat = new SimpleDateFormat("yyyy-MM-dd'T'hh:mm:ss'Z'");
@@ -42,6 +52,22 @@ public class GithubQuerier {
             sb.append(" on ");
             sb.append(formatted);
             sb.append("<br />");
+            // Add commits
+            sb.append("<h4> Commits: </h4>");
+            for(int k = 0; k < commits.length(); k++) {
+                sb.append("<i>sha ");
+                sb.append(k+1);
+                sb.append("</i>");
+                sb.append(": ");
+                sb.append(sha.get(k));
+                sb.append("<br />");
+                sb.append("<i>message ");
+                sb.append(k+1);
+                sb.append("</i>");
+                sb.append(": ");
+                sb.append(message.get(k));
+                sb.append("<br /><br />");
+            }
             // Add collapsible JSON textbox (don't worry about this for the homework; it's just a nice CSS thing I like)
             sb.append("<a data-toggle=\"collapse\" href=\"#event-" + i + "\">JSON</a>");
             sb.append("<div id=event-" + i + " class=\"collapse\" style=\"height: auto;\"> <pre>");
@@ -54,13 +80,20 @@ public class GithubQuerier {
 
     private static List<JSONObject> getEvents(String user) throws IOException {
         List<JSONObject> eventList = new ArrayList<JSONObject>();
+        JSONObject event;
         String url = BASE_URL + user + "/events";
         System.out.println(url);
         JSONObject json = Util.queryAPI(new URL(url));
         System.out.println(json);
         JSONArray events = json.getJSONArray("root");
-        for (int i = 0; i < events.length() && i < 10; i++) {
-            eventList.add(events.getJSONObject(i));
+        for (int i = 0; i < events.length(); i++) {
+            event = events.getJSONObject(i);
+            if(event.getString("type").equals("PushEvent")) {
+                eventList.add(event);
+            }
+            if(eventList.size() == 10) {
+                return eventList;
+            }
         }
         return eventList;
     }
